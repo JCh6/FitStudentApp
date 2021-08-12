ed73fc480540a845ef2c66dee9fd2f9279f1ce66
JCh6 <juanch@outlook.com>
2021-08-12 10:56:30 -0500

    first commit

---
+++ fitstudentapp/src/main/java/com/fit/juanchaparro/fitstudentapp/FitstudentappApplication.java
@@ -0,0 +1,13 @@
+package com.fit.juanchaparro.fitstudentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitstudentappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitstudentappApplication.class, args);
+	}
+
+}
